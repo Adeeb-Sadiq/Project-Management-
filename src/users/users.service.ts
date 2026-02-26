3d2ed9c59290021a43e868e02100b7021e6385ce
Abhishek8503 <abhishekg9000@gmail.com>
2026-02-26 18:57:21 +0530

    clearing out the comments.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
-// import { Prisma } from 'generated/prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 @Injectable()
 export class UsersService {
     
     constructor(private readonly databaseService: DatabaseService) {}
-
-    // private users = [
-    //   { id: 1, name: "Alice Johnson", email: "alice.johnson@example.com", role: "Admin" },
-    //   { id: 2, name: "Brian Smith", email: "brian.smith@example.com", role: "Dev" },
-    //   { id: 3, name: "Carla Martinez", email: "carla.martinez@example.com", role: "Intern" },
-    //   { id: 4, name: "David Lee", email: "david.lee@example.com", role: "Dev" },
-    //   { id: 5, name: "Emma Wilson", email: "emma.wilson@example.com", role: "Admin" },
-    //   { id: 6, name: "Frank Brown", email: "frank.brown@example.com", role: "Dev" },
-    //   { id: 7, name: "Grace Taylor", email: "grace.taylor@example.com", role: "Intern" },
-    //   { id: 8, name: "Henry Anderson", email: "henry.anderson@example.com", role: "Dev" },
-    //   { id: 9, name: "Isabella Thomas", email: "isabella.thomas@example.com", role: "Intern" },
-    //   { id: 10, name: "Jack White", email: "jack.white@example.com", role: "Admin" }
-    // ];
-
-    // private id = 11;
 
     async getUsers(role?: string) {
         if(role) {
