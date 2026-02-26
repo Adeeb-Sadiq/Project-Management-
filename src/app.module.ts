f570e86720650d56bd61b1d3f14135d74b497733
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-02-26 13:38:51 +0530

    database service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TasksModule, UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
